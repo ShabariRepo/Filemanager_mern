62b2cae5b04eaba0a4dd8f98c502472c3460817e
Shabari Shenoy <sshenoy@centrilogic.internal>
2019-10-23 16:58:15 -0400

    - add tset mysql to mysql db file

--- server/mysqldb.js
+++ server/mysqldb.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 
 // remote mysql db connection area
 var connection = mysql.createConnection({
-    host: '10.228.19.13',
+    host: '10.228.19.14',
     port: 3806,
     user: 'mysqladmin',
     password: 'cladmin',
