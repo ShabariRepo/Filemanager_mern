1584511a0bf766fbf45e836f2ad3179dabc4edb1
Shabari Shenoy <sshenoy@centrilogic.internal>
2020-03-16 18:06:07 -0400

    - facet for title

--- server/server.js
+++ server/server.js
@@ -614,10 +614,14 @@ getDistinctHashMap = async (req, res) => {
 router.get("/getKbs", (req, res) => {
   const { title } = req.body;
 // app.get("/getProduct", function(req, res) {
-  var strQuery = solrClient.query().q(`nid:*`).addParams({
-    field: 'title',
-    value: `${title}*`
+  var strQuery = solrClient.query().q(`nid:*`).facetQuery({
+    field: "title",
+    contains: `${title}`
   });
+  // .addParams({
+  //   field: 'title',
+  //   value: `${title}*`
+  // });
   solrClient.search(strQuery, function(err, result) {
     if (err) {
       console.log(err);
